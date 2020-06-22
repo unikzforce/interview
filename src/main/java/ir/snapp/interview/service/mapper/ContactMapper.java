e6c32d5de3b4eb2a915e76d9cc58e47787c0a7e3
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 22:19:25 +0430

    Contact WIP

--- src/main/java/ir/snapp/interview/service/mapper/ContactMapper.java
+++ src/main/java/ir/snapp/interview/service/mapper/ContactMapper.java
@@ -2,7 +2,14 @@ package ir.snapp.interview.service.mapper;
 
 import org.mapstruct.Mapper;
 
+import ir.snapp.interview.model.Contact;
+import ir.snapp.interview.service.dto.ContactDTO;
+
 @Mapper(componentModel = "spring", uses = {})
 public abstract class ContactMapper {
+	
+	public abstract ContactDTO convertToDTO(Contact contact);
+	
+	public abstract Contact covertToDomain(ContactDTO contactDTO);
 
 }
