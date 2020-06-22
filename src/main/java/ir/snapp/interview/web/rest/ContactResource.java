52566f29d1970d372f6ff85d52103d18b480fb68
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 22:02:07 +0430

    contact service WIP

--- src/main/java/ir/snapp/interview/web/rest/ContactResource.java
+++ src/main/java/ir/snapp/interview/web/rest/ContactResource.java
@@ -4,11 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ir.snapp.interview.service.ContactService;
 import ir.snapp.interview.service.dto.ContactDTO;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactResource {
+	
+	private final ContactService contactService;
+	
+	public ContactResource(ContactService contactService) {
+		this.contactService = contactService
+	}
 	
 	public ResponseEntity<ContactDTO> yes() {
 		
