2123b9501b25f30979364deff4f5ea5bb0a8d3a8
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 21:43:17 +0430

    spring boot web starter added

---
+++ src/main/java/ir/snapp/interview/web/rest/ContactResource.java
@@ -0,0 +1,10 @@
+package ir.snapp.interview.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactResource {
+
+}
