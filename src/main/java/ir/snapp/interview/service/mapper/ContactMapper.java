7e63ad59fcbde9f8b3d6b39d19749ab5c3007bc7
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 21:59:35 +0430

    mapstruct added

---
+++ src/main/java/ir/snapp/interview/service/mapper/ContactMapper.java
@@ -0,0 +1,8 @@
+package ir.snapp.interview.service.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {})
+public abstract class ContactMapper {
+
+}
--- src/main/java/ir/snapp/interview/web/rest/ContactResource.java
+++ src/main/java/ir/snapp/interview/web/rest/ContactResource.java
@@ -1,13 +1,18 @@
 package ir.snapp.interview.web.rest;
 
-//import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ir.snapp.interview.service.dto.ContactDTO;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactResource {
 	
-//	public ResponseEntity<>
+	public ResponseEntity<ContactDTO> yes() {
+		
+		return null;
+	}
 
 }
