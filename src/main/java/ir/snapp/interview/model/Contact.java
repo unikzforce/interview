0484d5f823bd3fafe41297998515c58942c18fd8
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 22:23:22 +0430

    adding fields to Contact and ContactDTO

--- src/main/java/ir/snapp/interview/model/Contact.java
+++ src/main/java/ir/snapp/interview/model/Contact.java
@@ -10,5 +10,65 @@ public class Contact {
 	@Id
 	@GeneratedValue
 	private long id;
+	
+	private String name;
+	
+	private String phoneNumber;
+	
+	private String email;
+	
+	private String organization;
+	
+	private String github;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getGithub() {
+		return github;
+	}
+
+	public void setGithub(String github) {
+		this.github = github;
+	}
+	
+	
 
 }
--- src/main/java/ir/snapp/interview/service/dto/ContactDTO.java
+++ src/main/java/ir/snapp/interview/service/dto/ContactDTO.java
@@ -1,5 +1,65 @@
 package ir.snapp.interview.service.dto;
 
 public class ContactDTO {
+	
+	private long id;
+	
+	private String name;
+	
+	private String phoneNumber;
+	
+	private String email;
+	
+	private String organization;
+	
+	private String github;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getGithub() {
+		return github;
+	}
+
+	public void setGithub(String github) {
+		this.github = github;
+	}
+	
 }
