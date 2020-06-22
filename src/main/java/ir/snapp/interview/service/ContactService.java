9dcebfa4d94fcc7f8deca85efd6c91111569cc60
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-22 22:09:02 +0430

    ContactService WIP

--- src/main/java/ir/snapp/interview/service/ContactService.java
+++ src/main/java/ir/snapp/interview/service/ContactService.java
@@ -2,10 +2,17 @@ package ir.snapp.interview.service;
 
 import org.springframework.stereotype.Service;
 
+import ir.snapp.interview.repository.ContactRepository;
 import ir.snapp.interview.service.dto.ContactDTO;
 
 @Service
 public class ContactService {
+	
+	private ContactRepository contactRepository;
+	
+	public ContactService(ContactRepository contactRepository) {
+		this.contactRepository = contactRepository;
+	}
 	
 	public ContactDTO createContact(ContactDTO newContact) {
 		return null;
--- src/main/java/ir/snapp/interview/web/rest/ContactResource.java
+++ src/main/java/ir/snapp/interview/web/rest/ContactResource.java
@@ -25,5 +25,7 @@ public class ContactResource {
 		
 		return ResponseEntity.ok(savedContact);
 	}
+	
+//	public ResponseEntity<>
 
 }
