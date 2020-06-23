8d6baa8707c71da102b218cbbf75b6a867106c09
unikzforce <hosein@xgdigitalsolutions.com>
2020-06-24 04:04:45 +0430

    reading Github Token from .env file

--- src/main/java/ir/snapp/interview/config/GithubConfiguration.java
+++ src/main/java/ir/snapp/interview/config/GithubConfiguration.java
@@ -4,17 +4,19 @@ import java.io.IOException;
 
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GithubConfiguration {
 
+	@Value("${GITHUB_PERSONAL_ACCESS_TOKEN}")
+	private String personalAccessToken;
+
 	@Bean
 	public GitHub githubClient() throws IOException {
-//		GitHubBuilder.fromEnvironment().withConnector(new HttpConn)
-		// TODO 
-		return GitHub.connect();
+		return new GitHubBuilder().withOAuthToken(personalAccessToken).build();
 	}
 	
 }
